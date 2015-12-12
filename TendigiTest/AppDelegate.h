262e8ec14d3e6df2cde42d935d0abbc60a65c54e
Roberto Cordon <roberto@robertos-macbook-pro.local>
2015-12-12 12:28:44 -0500

    Initial Commit

---
+++ TendigiTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TendigiTest
+//
+//  Created by Roberto Cordon on 12/12/15.
+//  Copyright © 2015 Roberto Cordon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TendigiTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TendigiTest
+//
+//  Created by Roberto Cordon on 12/12/15.
+//  Copyright © 2015 Roberto Cordon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
